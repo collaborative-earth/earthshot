42b79e8d0acd3b8c838c612a01b56b638593a3c8
Connor Richards <c.william.richards@gmail.com>
2021-01-30 18:49:46 -0800

    STY: black-en

--- earthshot/setup.py
+++ earthshot/setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="earthshot", # Replace with your own username
+    name="earthshot",  # Replace with your own username
     version="0.0.1",
     author="Earthshot Labs",
     author_email="author@example.com",
@@ -18,5 +18,5 @@ setuptools.setup(
         "License :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires=">=3.6",
 )
--- earthshot/water/water_viz.py
+++ earthshot/water/water_viz.py
@@ -1,5 +1,5 @@
-from bokeh.io import output_notebook 
-from bokeh.plotting import figure, output_file, show 
+from bokeh.io import output_notebook
+from bokeh.plotting import figure, output_file, show
 from bokeh.palettes import brewer, colorblind
 import ee
 import folium
@@ -10,14 +10,14 @@ _ = output_notebook()
 
 # Define a method for displaying Earth Engine image tiles to folium map.
 def add_ee_layer(self, ee_image_object, vis_params, name):
-  map_id_dict = ee.Image(ee_image_object).getMapId(vis_params)
-  folium.raster_layers.TileLayer(
-    tiles = map_id_dict['tile_fetcher'].url_format,
-    attr = 'Map Data &copy; <a href="https://earthengine.google.com/">Google Earth Engine</a>',
-    name = name,
-    overlay = True,
-    control = True
-  ).add_to(self)
+    map_id_dict = ee.Image(ee_image_object).getMapId(vis_params)
+    folium.raster_layers.TileLayer(
+        tiles=map_id_dict["tile_fetcher"].url_format,
+        attr='Map Data &copy; <a href="https://earthengine.google.com/">Google Earth Engine</a>',
+        name=name,
+        overlay=True,
+        control=True,
+    ).add_to(self)
 
 
 # Add EE drawing method to folium.
@@ -25,31 +25,33 @@ folium.Map.add_ee_layer = add_ee_layer
 
 
 def legend(
-        palette: tuple,
-        title: str='Legend Title (-)',
-        plot_width: int=500,
-        plot_height: int=75,
-        minimum: float=0,
-        maximum: float=1):
+    palette: tuple,
+    title: str = "Legend Title (-)",
+    plot_width: int = 500,
+    plot_height: int = 75,
+    minimum: float = 0,
+    maximum: float = 1,
+):
     pal_len = len(palette)
     pal_dist = maximum - minimum
-    width_color = pal_dist/pal_len
-    x_locs = [minimum + .5 * width_color * ((2 * ii) - 1)
-              for ii in range(pal_len+1) if ii != 0]
+    width_color = pal_dist / pal_len
+    x_locs = [
+        minimum + 0.5 * width_color * ((2 * ii) - 1)
+        for ii in range(pal_len + 1)
+        if ii != 0
+    ]
     # print(width_color)
     # print(x_locs)
 
-    legend = figure(
-        title=title, 
-        plot_width=plot_width, 
-        plot_height=plot_height)
-    
-    _ =legend.vbar(
-        x = x_locs,  
-        top = [0] * pal_len, 
-        bottom = [1] * pal_len, 
-        width = width_color, 
-        color = palette) 
+    legend = figure(title=title, plot_width=plot_width, plot_height=plot_height)
+
+    _ = legend.vbar(
+        x=x_locs,
+        top=[0] * pal_len,
+        bottom=[1] * pal_len,
+        width=width_color,
+        color=palette,
+    )
     show(legend)
     return None
 
