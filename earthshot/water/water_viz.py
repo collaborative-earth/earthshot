87a4f7296b3bf8aebed95b0b1fb5663feffa42d5
Connor Richards <c.william.richards@gmail.com>
2021-02-01 13:21:33 -0800

    FIX: remove merge conflict markers that Atom somehow decided to leave in

--- earthshot/water/water_viz.py
+++ earthshot/water/water_viz.py
@@ -10,16 +10,15 @@ _ = output_notebook()
 
 
 def add_ee_layer(self, ee_image_object, vis_params, name):
-  """A method for displaying or layering Earth Engine image tiles to folium map.
-  """
-  map_id_dict = ee.Image(ee_image_object).getMapId(vis_params)
-  folium.raster_layers.TileLayer(
-    tiles = map_id_dict['tile_fetcher'].url_format,
-    attr = 'Map Data &copy; <a href="https://earthengine.google.com/">Google Earth Engine</a>',
-    name = name,
-    overlay = True,
-    control = True
-  ).add_to(self)
+    """A method for displaying or layering Earth Engine image tiles to folium map."""
+    map_id_dict = ee.Image(ee_image_object).getMapId(vis_params)
+    folium.raster_layers.TileLayer(
+        tiles=map_id_dict["tile_fetcher"].url_format,
+        attr='Map Data &copy; <a href="https://earthengine.google.com/">Google Earth Engine</a>',
+        name=name,
+        overlay=True,
+        control=True,
+    ).add_to(self)
 
 
 # Add EE drawing method to folium.
@@ -28,13 +27,23 @@ folium.Map.add_ee_layer = add_ee_layer
 
 def legend(
     palette: tuple,
-<<<<<<< HEAD
     title: str = "Legend Title (-)",
     plot_width: int = 500,
     plot_height: int = 75,
     minimum: float = 0,
     maximum: float = 1,
-):
+) -> None:
+    """Create a stand-alone legend.
+    Args:
+        palette: tuple of colors
+        title: str for title
+        plot_width: int
+        plot_height: int
+        minimum: float of values to include
+        maximum: float of values to include
+    Returns:
+        None but displays a legend bar in a Jupyter Notebook.
+    """
     pal_len = len(palette)
     pal_dist = maximum - minimum
     width_color = pal_dist / pal_len
@@ -57,45 +66,6 @@ def legend(
     )
     show(legend)
     return None
-=======
-    title: str='Legend Title (-)',
-    plot_width: int=500,
-    plot_height: int=75,
-    minimum: float=0,
-    maximum: float=1) -> None:
-  """Create a stand-alone legend.
-  Args:
-      palette: tuple of colors
-      title: str for title
-      plot_width: int
-      plot_height: int
-      minimum: float of values to include
-      maximum: float of values to include
-  Returns:
-      None but displays a legend bar in a Jupyter Notebook.
-  """
-  pal_len = len(palette)
-  pal_dist = maximum - minimum
-  width_color = pal_dist/pal_len
-  x_locs = [minimum + .5 * width_color * ((2 * ii) - 1)
-            for ii in range(pal_len+1) if ii != 0]
-  # print(width_color)
-  # print(x_locs)
-
-  legend = figure(
-    title=title,
-    plot_width=plot_width,
-    plot_height=plot_height)
-
-  _ =legend.vbar(
-    x = x_locs,
-    top = [0] * pal_len,
-    bottom = [1] * pal_len,
-    width = width_color,
-    color = palette)
-  show(legend)
-  return None
->>>>>>> main
 
 
 # hmpf, no real gain
@@ -105,7 +75,7 @@ def folium_map(**kwargs) -> folium.Map:
 
 
 def folium_display(the_map) -> None:
-  """Display a folium map with layer control."""
-  the_map.add_child(folium.LayerControl())
-  display(the_map)
-  return None
+    """Display a folium map with layer control."""
+    the_map.add_child(folium.LayerControl())
+    display(the_map)
+    return None
