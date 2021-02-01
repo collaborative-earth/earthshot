a32bca35b4a9a29fec6bb8875e2004cb550cd801
Connor Richards <c.william.richards@gmail.com>
2021-02-01 13:40:52 -0800

    ENH: add missing asserts in test_basic_functionality

--- earthshot/tests/test_basic_functionality.py
+++ earthshot/tests/test_basic_functionality.py
@@ -4,8 +4,8 @@ import pytest
 
 
 def test_read_public_image_col(authenticate_gee):
-    isinstance(ee.ImageCollection("LANDSAT/LC08/C01/T1_SR"), ee.ImageCollection)
+    assert isinstance(ee.ImageCollection("LANDSAT/LC08/C01/T1_SR"), ee.ImageCollection)
 
 
 def test_read_public_feature_col(authenticate_gee):
-    isinstance(ee.FeatureCollection("TIGER/2016/States"), ee.FeatureCollection)
+    assert isinstance(ee.FeatureCollection("TIGER/2016/States"), ee.FeatureCollection)
