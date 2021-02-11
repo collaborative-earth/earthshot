ef48dfa86c8a55f764cab75b389b4e22a6c6dc30
James McCreight <jamesmcc@ucar.edu>
2021-02-10 22:20:11 -0700

    back in black

--- docs/conf.py
+++ docs/conf.py
@@ -12,14 +12,15 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+
+sys.path.insert(0, os.path.abspath(".."))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'Earthshot'
-copyright = '2021, Earthshot Labs Contributors'
-author = 'Earthshot Labs Contributors'
+project = "Earthshot"
+copyright = "2021, Earthshot Labs Contributors"
+author = "Earthshot Labs Contributors"
 
 
 # -- General configuration ---------------------------------------------------
@@ -28,33 +29,33 @@ author = 'Earthshot Labs Contributors'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.napoleon',
-    'sphinx.ext.viewcode',
-    'sphinx_rtd_theme',
-    'sphinxcontrib.apidoc',
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "sphinx_rtd_theme",
+    "sphinxcontrib.apidoc",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-apidoc_module_dir = '../earthshot'
-apidoc_output_dir = 'modules'
-apidoc_excluded_paths = ['tests']
+apidoc_module_dir = "../earthshot"
+apidoc_output_dir = "modules"
+apidoc_excluded_paths = ["tests"]
 apidoc_separate_modules = True
-apidoc_extra_args = ['-t', '_templates']
+apidoc_extra_args = ["-t", "_templates"]
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
